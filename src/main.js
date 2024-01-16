05ea48adfa427768b96260c0766ef3fbdca6d3b3
Trésor ILOYI <tresor.iloyi@gmail.com>
2024-01-16 16:10:21 +0100

    chore: setup Mozaic-Vue3

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import './assets/main.css'
+import '@mozaic-ds/vue-3/dist/mozaic-vue.css'
 
 import { createApp } from 'vue'
+import MozaicVue from '@mozaic-ds/vue-3'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(MozaicVue)
 
 app.mount('#app')
